ccf33e2da7c46588e172ed17c95814499605dd7d
Kije Williams <kijewilliams01@gmail.com>
2025-01-23 01:01:52 +0100

    Decove

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:react-native/all', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native'],
   rules: {
-    'prettier/prettier': 'error',
+    // Your custom rules
   },
 };
