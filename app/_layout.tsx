4484ef3f42fc707f7cacefd8985de7742d2df9ab
Thomas Ejembi <ejembithomas61@gmail.com>
2025-01-23 15:00:03 +0100

    set up native wind and native wind ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,20 @@
+import { colorScheme } from 'nativewind';
+import { useInitialAndroidBarSync, useColorScheme } from '../lib/useColorScheme';
+import { NAV_THEME } from '../theme';
 import AuthNavigation from './auth/_layout';
 import MainNavigation from './main/_layout';
+import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
 
 const Layout = () => {
+  useInitialAndroidBarSync();
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+
   return (
     <>
-      <AuthNavigation />
-      <MainNavigation />
+      <NavThemeProvider value={NAV_THEME[colorScheme]}>
+        <AuthNavigation />
+        <MainNavigation />
+      </NavThemeProvider>
     </>
   );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,102 @@
-import React from 'react';
-import { View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
 
-type Props = {};
+// type Props = {};
 // No text should be used directly. Please use the t from translation to access
 // the text save in the locales file.
 // this is to aid translation of the app
-export default function index({}: Props) {
-  const { t } = useTranslation();
+// export default function index({}: Props) {
+//   const { t } = useTranslation();
+//   return (
+//     <View>
+//       <Text>{t('home.header')}</Text>
+//     </View>
+//   );
+// }
+import { useHeaderHeight } from '@react-navigation/elements';
+import { Icon } from '@roninoss/icons';
+import { FlashList } from '@shopify/flash-list';
+import { cssInterop } from 'nativewind';
+import * as React from 'react';
+import { Linking, useWindowDimensions, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useColorScheme } from '../lib/useColorScheme';
+import { Text } from '../components/nativewindui/Text';
+
+cssInterop(FlashList, {
+  className: 'style',
+  contentContainerClassName: 'contentContainerStyle',
+});
+
+export default function Screen() {
   return (
-    <View>
-      <Text>{t('home.header')}</Text>
+    <FlashList
+      contentInsetAdjustmentBehavior="automatic"
+      keyboardShouldPersistTaps="handled"
+      data={COMPONENTS}
+      estimatedItemSize={200}
+      contentContainerClassName="py-4"
+      keyExtractor={keyExtractor}
+      ItemSeparatorComponent={renderItemSeparator}
+      renderItem={renderItem}
+      ListEmptyComponent={COMPONENTS.length === 0 ? ListEmptyComponent : undefined}
+    />
+  );
+}
+
+function ListEmptyComponent() {
+  const insets = useSafeAreaInsets();
+  const dimensions = useWindowDimensions();
+  const headerHeight = useHeaderHeight();
+  const { colors } = useColorScheme();
+  const height = dimensions.height - headerHeight - insets.bottom - insets.top;
+
+  return (
+    <View style={{ height }} className="flex-1 items-center justify-center gap-1 px-12">
+      <Icon name="file-plus-outline" size={42} color={colors.grey} />
+      <Text variant="title3" className="pb-1 text-center font-semibold">
+        No Components Installed
+      </Text>
+      <Text color="tertiary" variant="subhead" className="pb-4 text-center">
+        You can install any of the free components from the{' '}
+        <Text
+          onPress={() => Linking.openURL('https://nativewindui.com')}
+          variant="subhead"
+          className="text-primary">
+          NativeWindUI
+        </Text>
+        {' website.'}
+      </Text>
     </View>
   );
 }
+
+type ComponentItem = { name: string; component: React.FC };
+
+function keyExtractor(item: ComponentItem) {
+  return item.name;
+}
+
+function renderItemSeparator() {
+  return <View className="p-2" />;
+}
+
+function renderItem({ item }: { item: ComponentItem }) {
+  return (
+    <Card title={item.name}>
+      <item.component />
+    </Card>
+  );
+}
+
+function Card({ children, title }: { children: React.ReactNode; title: string }) {
+  return (
+    <View className="px-4">
+      <View className="gap-4 rounded-xl border border-border bg-card p-4 pb-6 shadow-sm shadow-black/10 dark:shadow-none">
+        <Text className="text-center text-sm font-medium tracking-wider opacity-60">{title}</Text>
+        {children}
+      </View>
+    </View>
+  );
+}
+
+const COMPONENTS: ComponentItem[] = [];
