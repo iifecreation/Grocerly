2310a29aaea75541a95601a0cac5575c71394271
Infinite-Concept <ifenowoifesegun@gmail.com>
2025-02-04 12:46:37 +0100

    implemented bottom tabs using expo-router

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -22,35 +22,35 @@ const Layout = () => {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name={APP_ROUTES.HOME}
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => <Octicons name="home" size={24} color={focused ? COLORS.light.primary : COLORS.light.grey3} />,
         }}
       />
       <Tabs.Screen
-        name="order"
+        name={APP_ROUTES.ORDER}
         options={{
           title: 'Order',
           tabBarIcon: ({ color, focused }) => <MaterialCommunityIcons name="truck-fast-outline" size={24} color={focused ? COLORS.light.primary : COLORS.light.grey3} />,
         }}
       />
       <Tabs.Screen
-        name="finance"
+        name={APP_ROUTES.FINANCE}
         options={{
           title: 'Finance',
           tabBarIcon: ({ color, focused }) => <Ionicons name="wallet-outline" size={24} color={focused ? COLORS.light.primary : COLORS.light.grey3} />,
         }}
       />
       <Tabs.Screen
-        name="cart"
+        name={APP_ROUTES.CART}
         options={{
           title: 'Cart',
           tabBarIcon: ({ color, focused }) => <Ionicons name="cart-outline" size={24} color={focused ? COLORS.light.primary : COLORS.light.grey3} />,
         }}
       />
       <Tabs.Screen
-        name="account"
+        name={APP_ROUTES.ACCOUNT}
         options={{
           title: 'Account',
           tabBarIcon: ({ color, focused }) => <Ionicons name="person-outline" size={24} color={focused ? COLORS.light.primary : COLORS.light.grey3} />,
